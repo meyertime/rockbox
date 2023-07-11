8e2e9f744ff632c8f78b260c3373a1e83b4f3a7b
David Meyer <david@meyertime.com>
2023-07-11 17:04:41 -0500

    Remind to update version

--- bins.pl
+++ bins.pl
@@ -1,5 +1,14 @@
 #!/usr/bin/perl
+
+print 'Did you remember to update the versions in tools/release/bins.pl? (y/n) ';
+chomp(my $answer = <STDIN>);
+if ($answer !~ /^y(es)?$/i) {
+    print "Aborting...\n";
+    exit 1;
+}
+
+print "Building...\n";
 use lib ".";
 $ENV{'PATH'} = $ENV{'PWD'} . '/tools/build/bin:' . $ENV{'PATH'};
-
+push(@ARGV, '-v');
 do './tools/release/bins.pl';
